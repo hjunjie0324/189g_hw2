af3ed48651256e172b21186bf00d697ae207c34c
Junjie <jiehan@ucdavis.edu>
2020-02-23 17:54:27 -0800

    Add files via upload

--- HW2-Junjie.R
+++ HW2-Junjie.R
@@ -16,34 +16,64 @@ singles[,2:20] <- movies[,6:24]
 u.big2<-merge(u.big,singles)
 
 colnames(u.big2)<-c('movienum','usernum','rating','transID','age','gender','occ','ZIP','unknown','Action','Adventure','Animation','Children','Comedy','Crime','Documentary','Drama','Fantasy','Noir','Horror','Musical','Mystery','Romance','SciFi','Thriller','War','Western')
-u.big2
-
 test<-sample(1:nrow(u.big2),5000)
 utest<-u.big2[test,]
 utrain<-u.big2[-test,]
+u.big2
 lmout<-lm(rating~age+gender+occ+unknown+Action+Adventure+Animation+Children+Comedy+Crime+Documentary+Drama+Fantasy+Noir+Horror+Musical+Mystery+Romance+SciFi+Thriller+War+Western,data=utrain)
 preds<-predict(lmout,utest)
-mean(abs(preds-utest$rating))    
+mean(abs(preds-utest$rating))    #0.91
 
 #new term when lm()rr/20
 u.big2[,"new"]<-NA
 mmin<-min(u.big2[,2])
 mmax<-max(u.big2[,2])
 
+newFea<-matrix(0L,nrow=mmax,ncol=19)
+
 for(i in (mmin:mmax)){
   uu<-u.big2[u.big2[,2]==i,]
-  rr<-t(as.matrix(colMeans(uu[,9:27])))%*%t(as.matrix(uu[,9:27]))
-  rr<-rr/19
-  u.big2[u.big2[,2]==i,28]<-t(rr)
+  for(j in (9:27)){
+    mean<-mean(uu[uu[,j]==1,]$rating)
+    newFea[i,j-8]<-mean
+  }
+  #rr<-t(as.matrix(colMeans(uu[,3])))%*%t(as.matrix(uu[,9:27]))
+  #rr<-rr/19
+  #u.big2[u.big2[,2]==i,28]<-t(rr)
 }
-
+newFea[is.nan(newFea)]<-0
+for(i in (1:nrow(u.big2))){
+  user_num<-u.big2[i,2]
+  rr<-newFea[user_num,]%*%t(as.matrix(u.big2[i,9:27]))
+  rr<-rr/19
+  u.big2[i,28]<-rr
+}
 
 test<-sample(1:nrow(u.big2),5000)
 utest<-u.big2[test,]
 utrain<-u.big2[-test,]
 lmout1<-lm(rating~age+gender+occ+unknown+Action+Adventure+Animation+Children+Comedy+Crime+Documentary+Drama+Fantasy+Noir+Horror+Musical+Mystery+Romance+SciFi+Thriller+War+Western+new,data=utrain)
 preds1<-predict(lmout1,utest)
-mean(abs(preds1-utest$rating))
+mean(abs(preds1-utest$rating))  #0.79
+
+user_mean<-tapply(u.big2$rating,u.big2$usernum,mean)
+movie_mean<-tapply(u.big2$rating,u.big2$movienum,mean)
+
+u.big2[,"user_mean"]<-NA
+u.big2[,"movie_mean"]<-NA
+for(i in (1:nrow(u.big2))){
+  userId<-u.big2[i,2]
+  movieId<-u.big2[i,1]
+  u.big2[i,29]<-user_mean[userId]
+  u.big2[i,30]<-movie_mean[movieId]
+}
+
+test<-sample(1:nrow(u.big2),5000)
+utest<-u.big2[test,]
+utrain<-u.big2[-test,]
+lmout1<-lm(rating~age+gender+occ+unknown+Action+Adventure+Animation+Children+Comedy+Crime+Documentary+Drama+Fantasy+Noir+Horror+Musical+Mystery+Romance+SciFi+Thriller+War+Western+new+user_mean+movie_mean,data=utrain)
+preds1<-predict(lmout1,utest)
+mean(abs(preds1-utest$rating))  #0.7
 
 u.big2[,"age_gender"]<-(u.big2$age)*(u.big2$gender)
 u.big2
